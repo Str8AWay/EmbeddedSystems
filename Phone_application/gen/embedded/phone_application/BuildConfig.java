11e3fb9b968697f4a83a2fc76bef98afa95f6aba
andrew-kirkham <andrew.kirkham@marquette.edu>
2013-09-07 10:27:02 -0500

    Added Hello World Applications

---
+++ Phone_application/gen/embedded/phone_application/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package embedded.phone_application;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Tablet_application/gen/embedded/tablet_application/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package embedded.tablet_application;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
