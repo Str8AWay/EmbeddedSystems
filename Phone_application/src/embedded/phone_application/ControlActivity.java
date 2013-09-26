eccadd925ef0cea79bf712df7634d0ef26a2d28e
andrew-kirkham <andrew.kirkham@marquette.edu>
2013-09-25 21:22:20 -0500

    updated android application
    added the ability to listen for udp packets

--- Phone_application/src/embedded/phone_application/ControlActivity.java
+++ Phone_application/src/embedded/phone_application/ControlActivity.java
@@ -3,19 +3,26 @@ package embedded.phone_application;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Arrays;
 
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 public class ControlActivity extends Activity {
 
 	String ip;
 	String port;
 	InetAddress local;
-	DatagramSocket socket;
+	DatagramSocket outSocket;
+	DatagramSocket inSocket;
+	DatagramPacket inPacket;
+	byte[] message = new byte[1500];
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +41,50 @@ public class ControlActivity extends Activity {
 		System.out.println(port);
 		
 		setupUDP();
+		sleep(500);
+		listenForMessage();
+	}
+	
+	public void sleep(int milliseconds){
+		try {
+			Thread.sleep(milliseconds);
+		}
+		catch (Exception e){
+			System.out.println("Sleep Failed");
+		}
+	}
+	
+	public void listenForMessage(){
+			try{
+				inSocket.receive(inPacket);
+				String text = new String(message, 0, inPacket.getLength());
+				inSocket.close();
+				if (text.length() > 0){
+					Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+					showMessage(text);
+				}
+				Arrays.fill(message, (Byte) null);
+			}
+			catch (Exception e){
+				System.out.println("socket error");
+				System.out.println(e.toString());
+				sleep(250);
+			}
+	}
+	
+	public void showMessage(String message) {
+		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+		alertDialog.setTitle("Message");
+		alertDialog.setMessage(message);
+		alertDialog.show();
 	}
 	
 	public void setupUDP(){
 		try{
-			socket = new DatagramSocket();
+			outSocket = new DatagramSocket();
 			local = InetAddress.getByName(ip);
+			inPacket = new DatagramPacket(message, message.length);
+			inSocket = new DatagramSocket(Integer.parseInt(port));
 		}
 		catch(Exception e){
 			System.out.println(e.toString());
@@ -47,12 +92,26 @@ public class ControlActivity extends Activity {
 	}
 	
 	public void stopRobot(View view){
-		String messageStr = "STOP";
+		sendMessage("STOP");
+	}
+	public void leftRobot(View view){
+		sendMessage("LEFT");
+	}
+	public void rightRobot(View view){
+		sendMessage("RIGHT");
+	}
+	public void forwardRobot(View view){
+		sendMessage("BACKWARDS");
+	}
+	public void backwardsRobot(View view){
+		sendMessage("FORWARDS");
+	}
+	public void sendMessage(String messageStr){
 		byte[] message = messageStr.getBytes();
 		int msg_length=messageStr.length();
 		DatagramPacket packet = new DatagramPacket(message, msg_length,local,Integer.parseInt(port));
 		try{
-			socket.send(packet);
+			outSocket.send(packet);
 		}
 		catch(Exception e){
 			System.out.println(e.toString());
