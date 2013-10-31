d29ef721032cdf66537080a686854c8364507b6f
Jenny Reiman <jennifer.reiman@marquette.edu>
2013-10-30 22:51:20 -0500

    Updated Deliverable and Communication

---
+++ Deliverables/Iteration 2/Code Files/communication/Debug/src/communication.d
@@ -0,0 +1,34 @@
+src/communication.d src/communication.o: ../src/communication.cpp \
+  ../src/motor.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/qeservo.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/singleton.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/9302hw.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/memmap.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/sonar.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/qegpioint.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/property.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/sonarcont.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/sonar.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/qegpioint.h
+
+../src/motor.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/qeservo.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/singleton.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/9302hw.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/memmap.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/sonar.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/qegpioint.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/property.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/sonarcont.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/sonar.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/qegpioint.h:
---
+++ Deliverables/Iteration 2/Code Files/communication/Debug/src/motor.d
@@ -0,0 +1,15 @@
+src/motor.d src/motor.o: ../src/motor.cpp ../src/motor.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/qeservo.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/singleton.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/9302hw.h \
+  /usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/memmap.h
+
+../src/motor.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/qeservo.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/singleton.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/9302hw.h:
+
+/usr/local/terkos/arm/arm-oe-linux-uclibcgnueabi/include/terk/memmap.h:
